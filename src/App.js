90e6bc9b6047074a6e0e2727cc74afe64e64c7a3
Thanat <bztzn512@gmail.com>
2021-11-02 22:19:10 -0400

    TeneryOperator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ShortCircuit from "./short-circuit-evaluation/ShortCircuit";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <ShortCircuit />
+    </div>
+  );
 }
 
 export default App;
--- src/short-circuit-evaluation/ShortCircuit.js
+++ src/short-circuit-evaluation/ShortCircuit.js
@@ -2,8 +2,25 @@ import React, { useState } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("");
-
-  return <></>;
+  const [isError, setError] = useState(true);
+  /*  const firstValue = text || "hello world";
+  const secondValue = text && "hello world"; */
+  const clickHandler = () => {
+    setError(!isError);
+  };
+  return (
+    <>
+      {/*   <h1>{firstValue}</h1>
+      <h1> value {secondValue}</h1> */}
+      {/*  <h1>{text || "jame"}</h1>
+      <h1>{!text && "Mike"}</h1> */}
+      <button className="btn" onClick={clickHandler}>
+        Click
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? "true" : "false"}
+    </>
+  );
 };
 
 export default ShortCircuit;
