c90fe45d07f43f148d9927a02fcab2d3ff68619a
Thanat <bztzn512@gmail.com>
2021-11-11 00:11:27 -0500

    count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropDrilling from "./propDrilling/PropDrilling";
+import Count from "./useReducer/countUseReducer/Count";
 
 function App() {
   return (
     <div className="container">
-      <PropDrilling />
+      <Count />
     </div>
   );
 }
--- src/useReducer/countUseReducer/Count.js
+++ src/useReducer/countUseReducer/Count.js
@@ -1,17 +1,31 @@
 import React, { useReducer } from "react";
 
-const reducer = () => {}
+const reducer = (state, action) => {
+  if (action.type === "decrement") {
+    return { count: state.count - 1 };
+  }
+  if (action.type === "increment") {
+    return { count: state.count + 1 };
+  }
+  if (action.type === "showName") {
+    return { state, name: "Mike" };
+  }
+};
 
-const initValue = {count: 0, name: ''};
+const initValue = { count: 0, name: "" };
 const Count = () => {
-  const [state, dispatch] = useReducer(reducer, initValue)
-  return  (
-  <>
-  <button onclick={}>-</button>
-  <button onClick={}>+</button>
-  <button onClick={}>click</button>
-  </>
-  )
+  const [state, dispatch] = useReducer(reducer, initValue);
+  return (
+    <>
+      <button onclick={() => dispatch({ type: "decrement" })}>-</button>
+      {state.count}
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <br />
+      <button onClick={() => dispatch({ type: "showName" })}>click</button>
+      <br />
+      {state.name}
+    </>
+  );
 };
 
 export default Count;
